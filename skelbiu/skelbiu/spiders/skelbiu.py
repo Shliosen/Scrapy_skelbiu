a725d3a71253c511a04274c46955bd7d8afe2a04
Shliosen <tv.vaitkus@gmail.com>
2020-02-10 17:04:04 +0200

    Gitacora saved 1 changed files

--- skelbiu/skelbiu/spiders/skelbiu.py
+++ skelbiu/skelbiu/spiders/skelbiu.py
@@ -17,7 +17,7 @@ class skelbimas(scrapy.Spider):
     def parse(self, response):
         page=response.url.split("/")[-2]
         filename= "skelbimai-%s.html" % page
-        with open (filename, 'wb') as f:
+        with open (filename, wb') as f:
             f.write(response.body)
         self.log('saved file %s' % filename)
 
