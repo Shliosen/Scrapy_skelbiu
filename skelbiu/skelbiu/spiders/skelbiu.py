fd81c98cf55b3c697ff38fba0be0c2c760dbba55
Shliosen <tv.vaitkus@gmail.com>
2020-02-10 17:09:11 +0200

    Gitacora saved 1 changed files

--- skelbiu/skelbiu/spiders/skelbiu.py
+++ skelbiu/skelbiu/spiders/skelbiu.py
@@ -6,7 +6,7 @@ class skelbimas(scrapy.Spider):
 
     def start_request(self):
         urls = [
-           'https://www.skelbiu.lt/skelbimai/paslaugos-darbas/darbo-paieska/it-specialistai/1/',
+           'https://wwwskelbiu.lt/skelbimai/paslaugos-darbas/darbo-paieska/it-specialistai/1/',
            'https://www.skelbiu.lt/skelbimai/paslaugos-darbas/darbo-paieska/it-specialistai/2/',
         ]
         for url in urls:
