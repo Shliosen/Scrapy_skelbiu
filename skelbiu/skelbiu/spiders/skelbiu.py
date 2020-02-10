9b02637267dbc2363e3eda0b513d252d4df1a6f7
Shliosen <tv.vaitkus@gmail.com>
2020-02-10 18:14:01 +0200

    Gitacora saved 1 changed files

--- skelbiu/skelbiu/spiders/skelbiu.py
+++ skelbiu/skelbiu/spiders/skelbiu.py
@@ -4,7 +4,7 @@ import scrapy
 class skelbimas(scrapy.Spider):
     name = 'sk'
 
-    def start_urls = [
+     start_urls = [
            'http://skelbiu.lt/skelbimai/paslaugos-darbas/darbo-paieska/it-specialistai/1',
            'http://skelbiu.lt/skelbimai/paslaugos-darbas/darbo-paieska/it-specialistai/2',
         ]
