5aca04856e74acf6b8092d8b8a1d73093aff2daa
Shliosen <tv.vaitkus@gmail.com>
2020-02-10 17:00:13 +0200

    Gitacora saved 1 changed files

--- skelbiu/skelbiu/spiders/skelbiu.py
+++ skelbiu/skelbiu/spiders/skelbiu.py
@@ -9,7 +9,7 @@ class skelbimas(scrapy.Spider):
            'https://www.skelbiu.lt/skelbimai/paslaugos-darbas/darbo-paieska/it-specialistai/1/',
            'https://www.skelbiu.lt/skelbimai/paslaugos-darbas/darbo-paieska/it-specialistai/2/',
         ]
-        for urls in urls:
+        for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
     def parse(self, response):
         page=response.url.split("/")[-2]
