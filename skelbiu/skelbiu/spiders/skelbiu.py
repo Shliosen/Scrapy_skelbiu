c56e73ffa5e8435a87578f27b0fac0beb4847953
Shliosen <tv.vaitkus@gmail.com>
2020-03-10 13:59:21 +0200

    Gitacora saved 1 changed files

--- skelbiu/skelbiu/spiders/skelbiu.py
+++ skelbiu/skelbiu/spiders/skelbiu.py
@@ -9,7 +9,18 @@ class skelbimas(scrapy.Spider):
            'http://skelbiu.lt/skelbimai/paslaugos-darbas/darbo-paieska/it-specialistai/2',
         ]
  
-    
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
             
 
